7f773af0a78008a1e106d1040bd27bf1c66cd982
Saksham Baronia <baronia.saksham@gmail.com>
2018-08-12 23:09:50 +0530

    path modified

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Car Inventory System</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
+  	<script src="../Scripts/app.js"></script>
+	</head>
+	<body>
+	<div class="container" >
+		<div class="content" style="text-align: center;" >
+		<h1 style="text-align: center;">Car Inventory System</h1>
+		<div style="align:center">
+			<a href="../public/Main/inventoryIndex.php" class="btn btn-primary btn-lg">Please click here to begin</a>
+		</div>
+	</div>
+	</div>
+</body>
+</html>
